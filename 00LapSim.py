cc9a07120a315e37fabb944c8caf344307c77028
u2berggeist <jrwrigh.iii@gmail.com>
2016-08-27 12:34:07 -0400

    Added Scratch for acceleration and braking physics
    Created function accelbalance that governs the acceleration physics of
    the car given a car and tire object, torque on the axle, and the initial
    velocity of the vehicle. This function is used as a argument into
    sp.integrate.quad which calculates the speed reached after a specific
    amount of time (it integrates from time=0 to a specified time).
    
    brakebalance function governs the physics of  braking. This is left as a
    second order ODE which is solved by sp.integrate.odeint.

--- 00LapSim.py
+++ 00LapSim.py
@@ -8,13 +8,16 @@ Created on Fri Jul 29 21:55:08 2016
 from Track import *
 from Car import *
 from Tire import *
+import Constants as const
+import numpy as np
+import scipy as sp
 
 
 
 
         
 corner1=Corner(7,19)
-tire=Tire(1.2)
+tire=Tire(1.2,0.4826)
 tiger19=Car(204,-1.204,0.567,0.608)
 
 print tiger19
@@ -22,3 +25,30 @@ print tiger19
 print corner1.V_max(tiger19,tire)
 print corner1.time(tiger19,tire)
 
+"""Straight calculation Scratch"""
+
+Tau=59 #N*m
+v0=14
+
+
+def accelbalance(t,car,tire,Tau,v0): #time,car,tire,Torque to tire,initial velocity
+    alpha=car.massv/(Tau*tire.radius)
+    beta=(2*car.massv)/(car.cd*const.rhoair*car.frontarea)
+    c=-(alpha*v0+beta/v0)    
+    return (((c-t)**2-4*alpha*beta)**0.5-c+t)/(2*alpha)
+    
+
+# (Tau=torque,alpha=alpha,beta=beta,v0=14)
+print sp.integrate.quad(accelbalance,0,1,args=(tiger19,tire,Tau,v0))
+
+
+def brakebalance(u,t,car,tire,v0):
+    u1, u2=u
+        #u1=x u2=x'
+    beta=(2*car.massv)/(car.cd*const.rhoair*car.frontarea)
+    gamma=(2*car.massv)/(car.cl*const.rhoair*car.frontarea)
+    uprime=[u2 , -tire.mu * sp.constants.g - ( 1/gamma + 1/beta ) * u2]
+    return uprime
+    
+    
+print sp.integrate.odeint(brakebalance,[0,14],np.linspace(0,2,100),args=(tiger19,tire,v0))
--- Tire.py
+++ Tire.py
@@ -6,5 +6,6 @@ Created on Tue Aug 02 18:47:38 2016
 """
 
 class Tire(object):
-    def __init__(self,mu):
+    def __init__(self,mu,radius):
         self.mu=mu
+        self.radius=radius
